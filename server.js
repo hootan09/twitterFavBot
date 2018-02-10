70b0a164f8fcff3f731dc9523e3666add19bb904
hootan <hootan09@gmail.com>
2018-02-10 11:50:13 +0330

    new

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ var error = function (err, response, body) {
     console.log('ERROR From twitter api: [%s]', err);
     console.log('response From twitter api: [%s]', response);
     console.log('error body From twitter api: [%s]', body);
-    io.emit('error' , err);
+    //io.emit('error' , err);
     io.emit('error' , response);
 };
 var FavBot = function (data) {
